974eda08a6e7acbd84725c87dc4044476f2c9b68
arne-source <arne@chmconstruction.net>
2026-02-24 12:48:24 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // Tell Vite that we are deploying to a GitHub Pages sub-directory
+  base: '/Bottle-filling-MPC/',
   plugins: [
     react(),
     tailwindcss(),
