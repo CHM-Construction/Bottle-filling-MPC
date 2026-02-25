d017fe59b0f3811cf5f2a80deb5c1294f0584364
arne-source <arne@chmconstruction.net>
2026-02-25 12:30:03 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ command }) => ({
-  // FIX: Automatically switch paths! '/' for local dev, '/Bottle-filling-MPC/' for GitHub
+  // FIX: Use '/' for local dev, but '/Bottle-filling-MPC/' for GitHub Pages!
   base: command === 'build' ? '/Bottle-filling-MPC/' : '/',
   plugins: [
     react(),
